888a153dcb96658e21140ac2694d4867c946639b
chain-lover <35804151+chain-lover@users.noreply.github.com>
2023-11-24 11:40:50 +0000

    chain style iterator progress

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,16 @@ where
     }
 }
 
+trait ProgressTraitExt: Sized {
+    fn progress(self) -> Progress<Self>;
+}
+
+impl<Iter> ProgressTraitExt for Iter where Iter: Iterator {
+    fn progress(self) -> Progress<Self> {
+        Progress::new(self)
+    }
+}
+
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
@@ -34,6 +44,10 @@ fn main() {
     let v = vec![1, 2, 3];
 
     for n in Progress::new(v.iter()) {
+        expensive_calculation(n);
+    }
+
+    for n in v.iter().progress() {
         expensive_calculation(n);
     }
 }
